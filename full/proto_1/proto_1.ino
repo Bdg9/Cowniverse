1c2f3859de58a0ec251856695489d2aceff63e34
Barbara <barbara.degroot@epfl.ch>
2024-04-16 17:33:09 +0200

    stepper et servos ensemble

--- full/proto_1/proto_1.ino
+++ full/proto_1/proto_1.ino
@@ -26,11 +26,13 @@ int gate_max = 270;
 
 //variables for stepper motor 
 int x;
-int trajet = 1;
+int trajet;
 int state_bigDome;
-int last_state_bigDome = HIGH;
+int last_state_bigDome = LOW;
 const int speed = 2000;
 const int bigDome = 2;
+const int nb_rev_trajet_1 = 1890;
+const int nb_rev_trajet_2 = 2065;
 
 
 void setup() {
@@ -41,17 +43,21 @@ void setup() {
   pwm.setPWMFreq(servoFrequency);          //Set the servo operating frequency
 
   // for stepper motor driver
-  pinMode(PUL, OUTPUT); 
-  pinMode(DIR, OUTPUT); 
+  pinMode(PUL,OUTPUT); // PUL
+  pinMode(DIR,OUTPUT); // DIR
   // for stepper motor button
   pinMode(bigDome, INPUT);
   // serial
-  // Serial.begin(9600);
-  // delay(3000);
-  // Serial.println(trajet);
+  Serial.begin(9600);
+  delay(3000);
+  // initialisation trajet n°1
+  trajet = 1;
+  Serial.println(trajet);
+  // direction of rotation
+  digitalWrite(DIR,LOW); // direction of rotation
 
   //puts servos at the right position
-  pwm.setPWM(DFR, 0, dr_min);
+  pwm.setPWM(DFR, 0, dr_max);
   delay(10);
   pwm.setPWM(DFL, 0, dl_min);
   delay(10);
@@ -64,18 +70,30 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-  open_front_doors();
-  delay(500);
-  close_front_doors();
-  delay(500);
-  open_hind_doors();
-  delay(500);
-  close_hind_doors();
-  delay(500);
-  open_gate();
-  delay(500);
-  close_gate();
-  delay(500);
+  state_bigDome = digitalRead(bigDome);
+
+  if (state_bigDome != last_state_bigDome) { // trajet 1
+    if ((trajet == 1) && (state_bigDome == HIGH)) {
+      open_hind_doors();
+      open_front_doors();
+      turn_stepper(nb_rev_trajet_1);
+      close_hind_doors();
+      trajet = 2;
+      state_bigDome = LOW;
+    }
+
+    if ((trajet == 2) && (state_bigDome == HIGH)) {
+      open_gate();
+      open_front_doors();
+      turn_stepper(nb_rev_trajet_2);
+      close_gate();
+      close_front_doors(); 
+      trajet = 1;    
+    }
+    delay(50);
+  }
+  
+  last_state_bigDome = state_bigDome;
 }
 
 //functions for house doors
@@ -126,5 +144,15 @@ void close_gate(){
     pwm.setPWM(GATE, 0, gate_max - i);
     delay(DELAY_SERVO);
   }
+}
+
+//functions stepper motor
+void turn_stepper(int nb_rev){
+  for(x = 0; x < nb_rev; x++) { // set number of revolution
+      digitalWrite(PUL,HIGH);  // set high level direction of rotation
+      delayMicroseconds(speed); // set rotate speed
+      digitalWrite(PUL,LOW); // output low
+      delayMicroseconds(speed);
+    }
 }
 
