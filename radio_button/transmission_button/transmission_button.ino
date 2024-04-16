56b70dd17715873dae9c864548ddfbefaf950185
Gonçalo Braga <goncalo.braga@epfl.ch>
2024-04-16 17:37:59 +0200

    upgraded the transmission code (radio params and last_state)

--- radio_button/transmission_button/transmission_button.ino
+++ radio_button/transmission_button/transmission_button.ino
@@ -1,16 +1,15 @@
 #include <SPI.h>
 #include <RF24.h>
-// The transmitter is the digispark
+// The transmitter is the digispark 
 // The pin numbers are the ones written on the ports 
 
-#define CE_PIN 9//3
-#define CSN_PIN 8//4
+#define CE_PIN 9 //3 //digispark
+#define CSN_PIN 8//4 //digispark
 //create an RF24 object
 RF24 radio(CE_PIN, CSN_PIN);  // CE, CSN
 
 const int pushButton = 5; // P5 is the only port left
-const int led = 5;
-
+int last_state;
 
 //address through which two modules communicate.
 const uint64_t address = "Butt1"; // any 5 character String or 40 bit address 
@@ -19,15 +18,22 @@ void setup()
 {
   Serial.begin(9600);
   if (!radio.begin()) {
-    while (1) {}  // hold in infinite loop
+    //while (1) {}  // hold in infinite loop
+    Serial.println("Could not turn radio on");
+  }else{
+    Serial.println("Radio successfully turned on");
   }
-  Serial.println("Successfull radio channel");
+  // radio settings
   radio.setPALevel(RF24_PA_LOW);
-  //set the address
-  radio.openWritingPipe(address);
+  radio.setPayloadSize(4); // sizeof("Meuh")
+  radio.setChannel(108); // 0 to 127
+  radio.setPALevel (RF24_PA_LOW); // for now //transmitter power level. To choose RF24_PA_MIN, RF24_PA_LOW, RF24_PA_HIGH, RF24_PA_MAX
+  radio.setDataRate (RF24_250KBPS); //exchange rate. To choose RF24_2MBPS, RF24_1MBPS, RF24_250KBPS
   
-  //Set module as transmitter
-  radio.stopListening();
+  //set the address for communication and set module as transmitter
+  radio.openWritingPipe(address);
+  radio.stopListening();//Set module as transmitter
+  
   pinMode(pushButton, INPUT);
 }
 
@@ -35,11 +41,17 @@ void loop()
 {
   int buttonState = digitalRead(pushButton);
   
-  const char text[] = "Button";
-  if(buttonState == HIGH){
-    radio.write(&text, sizeof(text));
-    Serial.println("Button pressed");
+  const char text[] = "Meuh";
+  if(buttonState != last_state){
+    if (buttonState == HIGH){
+      bool transmit = radio.write(&text, sizeof(text));
+      if(transmit){
+        Serial.println("Message Sent & Acknowledged");
+      }
+      Serial.println("Button pressed");
+    }
+    delay(50);
   }
-  delay(100);
-
-  }
+  last_state = buttonState;
+  
+}
